01bc21976761c8ee13d1a12d5905cab213cd46ec
LucyHN <lucy.h.nam@gmail.com>
2022-06-27 21:27:20 -0300

    Final

---
+++ src/Routes/Tv.tsx
@@ -0,0 +1,6 @@
+
+function Tv() {
+    return <h1></h1> ;
+}
+
+export default Tv;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import {RecoilRoot} from "recoil";
+import { createGlobalStyle } from "styled-components";
+import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+  @@ -55,27 +54,27 @@
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-weight: 300;
+    font-family: 'Source Sans Pro', sans-serif;
+    color:${(props) => props.theme.white.darker};
+    line-height: 1.2;
+    background-color: black;
+    height: 120vh;
+  }
+  a {
+    text-decoration:none;
+    color:inherit;
+  }
+`;
+
+const client = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <QueryClientProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />  
+        <App />
+      </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
